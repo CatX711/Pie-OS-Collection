79367fbc781d7d8394e084643e3b63ae2756b373
Daniel C <104099162+CatX711@users.noreply.github.com>
2023-01-03 22:49:50 +0000

    Add files via upload

---
+++ pie_os_7200_edition.py
@@ -0,0 +1,26 @@
+from time import sleep
+from playsound import playsound
+print("Booting...")
+sleep(2)
+print("Loading system44")
+sleep(4)
+playsound('Windows ME Startup.mp3')
+sleep(1)
+print("Welcome to Pie OS 7200 version!")
+sleep(1)
+priny("Loading interface...")
+sleep(2)
+print("""
+
+   YOU ARE CURRENTLY IN: HOME
+   ---------------------------
+   Here's some basic commands to get you ready!
+   
+   LSC - Shows a list of all commands.
+   @GAMES - Lets you choose what game you want to play. 
+   @WRD - Starts a basic text editor.
+   TMANAGER - Opens a task manager
+   
+
+
+""")
